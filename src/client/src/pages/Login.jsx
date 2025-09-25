c2989b5c4aaab8f647c95cc487933c0abe2819b8
Bolivar Alexis <alexiiz360@gmail.com>
2025-09-24 19:58:19 -0500

    create component and updates

--- src/client/src/pages/Login.jsx
+++ src/client/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = ({ setUser }) => {
   };
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 border-2 border-gray-300 rounded-lg shadow-md">
+    <div className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8 border-2 border-gray-300 rounded-lg shadow-md">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
           <img
             alt="Global Mind-Solutions"
@@ -78,6 +78,32 @@ const Login = ({ setUser }) => {
           </form>
         </div>
       </div>
+
+      /*
+<div class="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style="background-image:url('https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80')">
+  <div class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+    <div class="text-white">
+      <div class="mb-8 flex flex-col items-center">
+        <img src="../src/assets/gms_logo.jpg" width="150" alt="" srcset="" />
+        <h1 class="mb-2 text-2xl">Instagram</h1>
+        <span class="text-gray-300">Enter Login Details</span>
+      </div>
+      <form action="#">
+        <div class="mb-4 text-lg">
+          <input class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="text" name="name" placeholder="id@email.com" />
+        </div>
+
+        <div class="mb-4 text-lg">
+          <input class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="Password" name="name" placeholder="*********" />
+        </div>
+        <div class="mt-8 flex justify-center text-lg text-black">
+          <button type="submit" class="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+      */
 
   );
 };
